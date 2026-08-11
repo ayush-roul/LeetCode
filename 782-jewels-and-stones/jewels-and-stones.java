f93477da405417fce9b8111201d3962f0bd98a58
ayush-roul <147304084+ayush-roul@users.noreply.github.com>
2026-08-11 20:48:33 +0530

    Time: 2 ms (12.23%) | Memory: 43.3 MB (36.91%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelset=new HashSet<>();
+
+        for(int i=0;i<jewels.length();i++){
+            jewelset.add(jewels.charAt(i));
+        }
+
+        int score=0;
+        for(int i=0;i<stones.length();i++){
+           if( jewelset.contains(stones.charAt(i))){
+            score++;
+           }
+        }
+        return score;
+
+    }
+}
